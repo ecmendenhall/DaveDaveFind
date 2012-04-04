9c66fa70a9de4d58892ddc002b6b3bf0bbe8a006
Connor Mendenhall <ecmendenhall@gmail.com>
2012-04-04 19:58:28 +0300

    Added BeautifulSoup parsing.

--- crawler/udacity_crawler.py
+++ crawler/udacity_crawler.py
@@ -1,3 +1,6 @@
+from bs4 import BeautifulSoup
+import urllib
+
 def crawl_web(seed): # returns index, graph of inlinks
     tocrawl = [seed]
     crawled = []
@@ -6,35 +9,19 @@ def crawl_web(seed): # returns index, graph of inlinks
     while tocrawl: 
         page = tocrawl.pop()
         if page not in crawled:
-            content = get_page(page)
-            add_page_to_index(index, page, content)
-            outlinks = get_all_links(content)
+            soup = get_page(page)
+            add_page_to_index(index, page, soup)
+            outlinks = get_all_links(soup)
             graph[page] = outlinks
             union(tocrawl, outlinks)
             crawled.append(page)
     return index, graph
 
-
-def get_next_target(page):
-    start_link = page.find('<a href=')
-    if start_link == -1: 
-        return None, 0
-    start_quote = page.find('"', start_link)
-    end_quote = page.find('"', start_quote + 1)
-    url = page[start_quote + 1:end_quote]
-    return url, end_quote
-
 def get_all_links(page):
     links = []
-    while True:
-        url, endpos = get_next_target(page)
-        if url:
-            links.append(url)
-            page = page[endpos:]
-        else:
-            break
+    for link in page.find_all('a'):
+    	links.append(link.get('href'))
     return links
-
 
 def union(a, b):
     for e in b:
@@ -42,9 +29,10 @@ def union(a, b):
             a.append(e)
 
 def add_page_to_index(index, url, content):
-    words = content.split()
-    for word in words:
-        add_to_index(index, word, url)
+	text = content.get_text()
+	words = text.split()
+	for word in words:
+		add_to_index(index, word, url)
         
 def add_to_index(index, keyword, url):
     if keyword in index:
@@ -64,8 +52,8 @@ def get_page(url):
     else:
         print "Page not in cache: " + url
         try: 
-        	import urllib
-        	return urllib.urlopen(url).read()
+        	content = urllib.urlopen(url).read()
+        	return BeautifulSoup(content)
         except:
         	return ""
         	
